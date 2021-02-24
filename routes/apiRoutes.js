8cfb320c300c46d8bf0ae630907289a898627acb
Colin Mudie <csmudie1@gmail.com>
2021-02-24 15:28:09 -0500

    fs writing json data to db.json

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,10 +1,16 @@
-const noteData = [
-    {
-        noteTitle: 'Hello World!',
-        noteText: 'hello world but smaller'
-    }
-]
+const db = require('../db/db.json');
+const fs = require('fs');
 
 module.exports = (app) => {
-    app.get('/api/notes', (req, res) => res.json(noteData));
-}
+    app.get('/api/notes', (req, res) => res.json(db));
+
+    app.post('/api/notes', (req, res) => {
+        db.push(req.body);
+        res.json(true);
+        console.log(__dirname + '../');
+        console.log(db);
+        
+        fs.writeFile('../Note_Taker/db/db.json', JSON.stringify(db), (err) => 
+        err ? console.log(err) : console.log('written data to db.json'));
+    });
+};
